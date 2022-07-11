567fb59eded90fc6e17bc6926e8a6ccd40f97923
koluoch1 <koluoch41@gmail.com>
2022-07-11 06:32:14 -0700

    prints a string, followed by a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *
+ * _puts -> this is a function that puts
+ *  @str: a param to puts function
+ *
+ */
+
+void puts(char *str)
+
+{
+
+for (; *str != '\0'; str++)
+{
+_putchar(*str);
+}
+_putchar('\n');
